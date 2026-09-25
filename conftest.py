7e6462d1fc95da4dca02df0f1d433594d8df4654
govind <govind.kumit@gmail.com>
2026-09-25 18:48:10 +0530

    Fix Selenium Chrome for CI

--- conftest.py
+++ conftest.py
@@ -1,11 +1,21 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
 def driver():
+    options = Options()
 
-    driver = webdriver.Chrome()
+    # Required for GitHub Actions / CI
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    # Useful for CI environments
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
 
     yield driver
 
